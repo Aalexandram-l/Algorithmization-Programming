cd3969e205542c3479b80c9107b6c19a5c22cae0
Aalexandram-l <aleksandracasina475@gmail.com>
2025-05-25 19:14:02 +0600

    Add files via upload

---
+++ Lb10.cs
@@ -0,0 +1,81 @@
+﻿/*
+Лабораторная работа 10
+14.04.2025
+база данных библиотека в которой книга описана следующей структурой
+фио, наименование, год выпуска, издательство
+необходимо сделать выборку, какая книга никогда не выдовалась читателю и какие книги находятся на руках
+у каждой книги есть бланк с датами выдачи и сдачи 
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+ 
+        List<Book> books = new List<Book>
+        {
+            new Book("Толстой Л.Н.", "Война и мир", 1869, "Эксмо"),
+            new Book("Достоевский Ф.М.", "Преступление и наказание", 1866, "АСТ"),
+            new Book("Булгаков М.А.", "Мастер и Маргарита", 1966, "Азбука")
+        };
+
+        List<BookRecord> records = new List<BookRecord>
+        {
+            new BookRecord(books[0], new DateTime(2023, 1, 10), new DateTime(2023, 2, 15)),
+            new BookRecord(books[0], new DateTime(2023, 3, 1), null), 
+            new BookRecord(books[2], new DateTime(2023, 2, 5), new DateTime(2023, 3, 10))
+        };
+
+        var neverBorrowed = books.Where(b => !records.Any(r => r.Book == b)).ToList();
+
+        var onHands = records.Where(r => r.RetDate == null).Select(r => r.Book).ToList();
+
+        Console.WriteLine("Никогда не выдавались:");
+        foreach (var book in neverBorrowed)
+        {
+            Console.WriteLine($"{book.Author} - {book.Title}");
+        }
+
+        Console.WriteLine("\nСейчас на руках:");
+        foreach (var book in onHands)
+        {
+            Console.WriteLine($"{book.Author} - {book.Title}");
+        }
+    }
+}
+
+class Book
+{
+    public string Author { get; set; }
+    public string Title { get; set; }
+    public int Year { get; set; }
+    public string Publisher { get; set; }
+
+    public Book(string author, string title, int year, string publisher)
+    {
+        Author = author;
+        Title = title;
+        Year = year;
+        Publisher = publisher;
+    }
+}
+
+class BookRecord
+{
+    public Book Book { get; set; }
+    public DateTime CheckDate { get; set; }
+    public DateTime? RetDate { get; set; }
+
+    public BookRecord(Book book, DateTime checkDate, DateTime? retDate)
+    {
+        Book = book;
+        CheckDate = checkDate;
+        RetDate = retDate;
+    }
+}
+*/
+
---
+++ Lb11.cs
@@ -0,0 +1,56 @@
+﻿/*Лабороторная работа 11
+21.04.2025
+дан входной файл состоящий из набора строк 
+необходимо создать выходной файл в который поместить строки из входного файла, в которых имеется хотябы одно четное число
+число считаем поледовательность цифр ограниченных другими иными знаками 
+
+
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputFile = "input.txt";
+        string outputFile = "output.txt";
+
+        try
+        {
+
+            string[] lines = File.ReadAllLines(inputFile);
+
+            var resultLines = new System.Collections.Generic.List<string>();
+
+            Regex numberRegex = new Regex(@"-?\d+");
+
+            foreach (string line in lines)
+            {
+                MatchCollection matches = numberRegex.Matches(line);
+
+                foreach (Match match in matches)
+                {
+                    if (int.TryParse(match.Value, out int number))
+                    {
+                        if (number % 2 == 0)
+                        {
+                            resultLines.Add(line);
+                            break; 
+                        }
+                    }
+                }
+            }
+
+            File.WriteAllLines(outputFile, resultLines);
+
+            Console.WriteLine($"Обработка завершена. Результат записан в {outputFile}");
+            Console.WriteLine($"Найдено строк с четными числами: {resultLines.Count}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+    }
+}
+*/
